58ca38472e531fa502124f7bc7fcdadd8b451751
Stylon Wang <stylon.wang@avermedia.com>
2014-10-07 11:01:49 +0800

    Add header guard.
    	modified:   alsa.h

--- alsa.h
+++ alsa.h
@@ -1,4 +1,8 @@
+#ifndef __FFT_ALSA_H__
+#define __FFT_ALSA_H__
 
 int capture_setup (char *device, unsigned int desired_rate);
 void capture_finish ();
 int capture_audio (short *buf, int data_size);
+
+#endif //__FFT_ALSA_H__
